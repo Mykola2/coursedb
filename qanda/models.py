c3f20fd62ab73af0d5d51b8d4bfa50c5224e9d22
Michiganx <michigan@ukr.net>
2014-11-28 23:41:01 +0200

    testing this bitch

--- qanda/models.py
+++ qanda/models.py
@@ -4,3 +4,8 @@ class Mymodel(models.Model):
     modname = models.CharField(max_length=30)
     asd = models.CharField(max_length=666)
     asd1 = models.CharField(max_length=666)
+
+
+class  tts(Mymodel, list):
+    #just another test model
+    pass
