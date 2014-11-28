bc2b2e241856c24f1fa36bf06447451059ceaa6f
Mykola2 <nick.ohoiko@gmail.com>
2014-11-29 00:59:34 +0200

    fields

--- qanda/models.py
+++ qanda/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
-class Mymodel(models.Model):
-    modname = models.CharField(max_length=30)
-    asd = models.CharField(max_length=666)
-    asd1 = models.CharField(max_length=666)
 
 
-class  tts(Mymodel, list):
-    #just another test model
-    pass
+class User(models.Model):
+    login = models.CharField(145)
+    pwd = models.CharField(145)
+    class Meta:
+        managed = False
+        db_table = 'user'
+
+class Answer(models.Model):
+    content = models.TextField()
+    postdate = models.DateField()
+    user_iduser = models.ForeignKey('User',db_column='user_iduser')
+    question_idquestion = models.ForeignKey('Question', db_column='question_idquestion')
+    likes = models.ManyToManyField(User)
+    class Meta:
+        managed = False
+        db_table = 'answer'
+
+class Tag(models.Model):
+    name = models.CharField(65)
+    class Meta:
+        managed = False
+        db_table = 'tag'
+
+class Question(models.Model):
+   title = models.TextField()
+   content = models.TextField()
+   postdate = models.DateField()
+   user_iduser = models.ForeignKey('User',db_column='user_iduser')
+   likes = models.ManyToManyField(User) #???
+   tags = models.ManyToManyField(Tag)
+   class Meta:
+        managed = False
+        db_table = 'question'
