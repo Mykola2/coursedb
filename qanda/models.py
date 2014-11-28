138fe345bc1f4c3f80d01b35835d567ca129c926
Mykola2 <nick.ohoiko@gmail.com>
2014-11-28 22:26:10 +0200

    fields

--- qanda/models.py
+++ qanda/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Mymodel(models.Model):
     modname = models.CharField(max_length=30)
     asd = models.CharField(max_length=666)
+    asd1 = models.CharField(max_length=666)
