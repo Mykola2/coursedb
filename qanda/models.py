40db1825c6c6f748f0367781e0e020ae1ef33e92
Mykola2 <nick.ohoiko@gmail.com>
2014-11-29 17:49:24 +0200

    Models_upd

--- qanda/models.py
+++ qanda/models.py
@@ -3,35 +3,27 @@ from django.db import models
 
 
 class User(models.Model):
-    login = models.CharField(145)
-    pwd = models.CharField(145)
-    class Meta:
-        managed = False
-        db_table = 'user'
+    login = models.CharField(max_length=145)
+    pwd = models.CharField(max_length=145)
+
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Question(models.Model):
+   title = models.TextField()
+   content = models.TextField()
+   postdate = models.DateField()
+   user_iduser = models.ForeignKey('User', db_column='user_iduser')
+   likes = models.ManyToManyField(User,related_name="question_likes") #???
+   tags = models.ManyToManyField(Tag,related_name="question_tags")
 
 class Answer(models.Model):
     content = models.TextField()
     postdate = models.DateField()
     user_iduser = models.ForeignKey('User',db_column='user_iduser')
     question_idquestion = models.ForeignKey('Question', db_column='question_idquestion')
-    likes = models.ManyToManyField(User)
-    class Meta:
-        managed = False
-        db_table = 'answer'
-
-class Tag(models.Model):
-    name = models.CharField(65)
-    class Meta:
-        managed = False
-        db_table = 'tag'
-
-class Question(models.Model):
-   title = models.TextField()
-   content = models.TextField()
-   postdate = models.DateField()
-   user_iduser = models.ForeignKey('User',db_column='user_iduser')
-   likes = models.ManyToManyField(User) #???
-   tags = models.ManyToManyField(Tag)
-   class Meta:
-        managed = False
-        db_table = 'question'
+    likes = models.ManyToManyField(User, related_name="answer_likes")
