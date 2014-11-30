edc6d6ba4de2d49bed366d644e421e8d02d06882
Michiganx <michigan@ukr.net>
2014-12-01 00:48:08 +0200

    правки к HTML-кам

--- qanda/views.py
+++ qanda/views.py
@@ -3,20 +3,21 @@ from qanda.models import *
 
 
 def add(request):
-    q = Question()
-    q.title = request.GET['title']
-    q.content = request.GET['content']
-    # пользователь который задал  вопрос блеать!
-    # нужно решить вопрос срегой и аутентификацией
-    q.save()
-    tags_list = [Tag(name=t.strip()) for t in request.GET['tags'].sprit(',')]
-    for tag in tags_list:
-        tag.save()
-    # тут будет tрабла - дублирующиеся теги будут повторяться
-    # и снова записываться в базу
-    # ИСПРАВИТЬ!!11!
-        q.tags.add(tag)
-    q.save()
+    if request.GET:
+        q = Question()
+        q.title = request.GET['title']
+        q.content = request.GET['content']
+        # пользователь который задал  вопрос блеать!
+        # нужно решить вопрос с регой и аутентификацией
+        q.save()
+        tags_list = [Tag(name=t.strip()) for t in request.GET['tags'].sprit(',')]
+        for tag in tags_list:
+            tag.save()
+        # тут будет tрабла - дублирующиеся теги будут повторяться
+        # и снова записываться в базу
+        # ИСПРАВИТЬ!!11!
+            q.tags.add(tag)
+        q.save()
     return redirect(r'/') # нужен редирект на details нового вопроса
 
 
